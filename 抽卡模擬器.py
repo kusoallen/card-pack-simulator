e11cb1ff10ba1911446e90731443b4b96ce7bd12
kusoallen <74462798+kusoallen@users.noreply.github.com>
2025-04-03 00:28:58 +0800

    Update 抽卡模擬器.py

--- 抽卡模擬器.py
+++ 抽卡模擬器.py
@@ -347,7 +347,7 @@ def show_card_images_with_animation(card_df):
     }}
     @keyframes pulse {{
         0%   {{ transform: scale(1); }}
-        50%  {{ transform: scale(1.1); }}
+        50%  {{ transform: scale(1.5); }}
         100% {{ transform: scale(1); }}
     }}
     .pulse-animation {{
